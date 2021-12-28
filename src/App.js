c8ad7c47a7a2b6c49c5278b9d3f17731c3d32324
Zulharmin-192113301 <192113301@students.mikroskil.ac.id>
2021-12-28 17:56:23 +0700

    Latihan 4.4 - Nambah data array dan update state

--- src/App.js
+++ src/App.js
@@ -1,51 +1,107 @@
 //App.js
 
 import React, { Component } from 'react';
-import './App.css';
+import './css/bootstrap.min.css';
 
 class App extends Component {
+  constructor(){
+  super();
+
+  this.state = {
+    info : [],
+    nama : null,
+    kelas : null,
+    nilai : null
+  }
+}
+
+  setvalueState (event) {
+    this.setState({
+      [event.target.name] : event.target.value
+    })
+  }
+
+  addData() {
+    var data_tmp = this.state.info;
+    data_tmp.push({nama : this.state.nama, kelas : this.state.kelas, niai : this.state.nilai});
+    this.setState ({
+      info : data_tmp
+    })
+  }
+  
 
   render() {
-    var infoumur = [
-      {nama:'Budi',umur:54},
-      {nama:'Doni',umur:23},
-      {nama:'Yanto',umur:41},
-      {nama:'Desi',umur:30}
-    ]
-    
+   
   return (
-    <table border='2'>
-      <thead>
-        <tr>
-          <th>
-            No.
-          </th>
-          <th>
-            Nama
-          </th>
-          <th>
-            Umur
-          </th>
-        </tr>
-      </thead>
-      <tbody>
 
-        {infoumur.map((item,index) =>(
-          <tr key={index}>
-            <td>
-              {index +1}
-            </td>
-            <td>
-              {item.nama}
-            </td>
-            <td>
-              {item.umur}
-            </td>
+    <div className='container'>
+      <div className='form-container'>
+        <div className='form-group'>
+          <label>Nama:</label>
+          <input type='text' name='nama' value={this.state.nama} onChange={this.setvalueState.bind(this)} className='form-control' />
+        </div>
+
+        <div className='form-group'>
+          <label>Kelas:</label>
+          <input type='text' name='kelas' value={this.state.kelas} onChange={this.setvalueState.bind(this)} className='form-control' />
+        </div>
+
+        <div className='form-group'>
+          <label>Nilai:</label>
+          <input type='number' name='nilai' value={this.state.nilai} onChange={this.setvalueState.bind(this)} className='form-control' />
+        </div>
+
+        <button onClick={this.addData.bind(this)} type='button' className='btn btn-success'> Masukkan</button>
+      </div>
+
+      <br/>
+  {/*cetak info siswa */}
+
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>
+              No.
+            </th>
+            <th>
+              Nama
+            </th>
+            <th>
+              Kelas
+            </th>
+            <th>
+              Nilai
+            </th>
           </tr>
-        ))}
+        </thead>
 
-      </tbody>
-    </table>
+        <tbody>
+            {this.state.info.map((info, index)=> (
+              <tr key={index}>
+                <td>
+                  {index +1}
+                </td>
+                <td>
+                  {info.nama}
+                </td>
+                <td>
+                  {info.kelas}
+                </td>
+                <td>
+                  {info.nilai}
+                </td>
+              </tr>
+            ))}
+            
+        </tbody>
+
+
+
+
+      </table>
+
+    </div>
+
   )
 }
 }
